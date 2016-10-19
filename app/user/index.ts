41b7b3f9384e8329880a5fb38b83b2d6eef52f41
Sean G. Wright <sean@seangwright.me>
2016-10-18 22:05:47 -0400

    fix(UserModule): typo in barrel export

--- app/user/index.ts
+++ app/user/index.ts
@@ -1,3 +1,3 @@
-export * from "./components";
-export * from "./models";
-export * from "./services";
+export * from "./components/index";
+export * from "./models/index";
+export * from "./services/index";
